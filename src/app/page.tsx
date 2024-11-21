1b70ca3c2adc7610f8d6df5cdb8f538fc6f6124f
Artem Rasskazimov <john@WCR.local>
2024-11-21 23:08:49 +0100

    switched to next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
+import { DiscogWantlistResponse, DiscogWantlistRecord } from "@/lib/DiscogWantlist";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const discogsUrl = "https://api.discogs.com/users/sozhran/wants"
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const pageLimit = 12    // depends on the Wantlist size and may be changed later
+
+async function getDiscogsInfo() {
+	const records: DiscogWantlistRecord[] = []
+	
+	for (let i = 1; i < pageLimit; i ++) {
+		try {
+			const response = await fetch(`${discogsUrl}` + "?per_page=1050&sort=rating&sort_order=desc");
+			const data = await response.json() as DiscogWantlistResponse;
+
+			data.wants.map((item: DiscogWantlistRecord) => records.push(item))
+		}
+		catch (error) {
+			console.log(error)
+		}
+	}
+	return records
 }
+
+export default async function Home() {
+	const records = await getDiscogsInfo()
+	return <>
+			<section className="collection">
+				<h1>Total: {records ? records.length : "¯\_(ツ)_/¯"}</h1>
+				{records ? records.map((record: DiscogWantlistRecord, i) => <div key={"record_" + i}>{record.basic_information.id} - {record.basic_information.title}</div>) : <></>}
+			</section>
+			<section className="footer">
+			<a href="https:/github.com/sozhran/wantlister" target="_blank">
+				<img
+					alt="github share icon"
+					src="https://i.imgur.com/5Qr1cEC.png"
+					width="25"
+					height="25"
+				/>
+			</a>
+			</section>
+		</>
+	}
+
+
+
+// BATTLEPLAN
+// import basic Discogs data without auth by button
+// add auth
+// map basic data and cook list of rated records
+// request marketplace for these records, display on page
+// improved display, art, distinguish by date/time
+// ...
+// add database
---
+++ src/lib/DiscogWantlist.ts
@@ -0,0 +1,52 @@
+export interface DiscogWantlistResponse {
+	pagination: Pagination;
+	wants: DiscogWantlistRecord[];
+}
+
+export interface Pagination {
+	page: number;
+	pages: number;
+	per_page: number;
+	items: number;
+	urls: { "last": string, "next": string };
+}
+
+export interface DiscogWantlistRecord {
+	id: number;
+	resource_url: string;
+	date_added: string;
+	rating: number;
+	basic_information: BasicInformation;
+}
+  
+export interface BasicInformation {
+	id: number;
+	master_id?: number;
+	master_url: string;
+	resource_url: string;
+	thumb: string;
+	cover_image: string;
+	title: string;
+	year: number;
+	genres: string[];
+	styles: string[];
+	artists: Artist[];
+	formats: Formats[];
+  }
+  
+interface Formats {
+	descriptions: string[];
+	name: string;
+	qty: string;
+	text?: string;
+  }
+  
+export interface Artist {
+	anv: string;
+	id: string;
+	name: string;
+	resource_url: string;
+	role: string;
+	tracks: string;
+	join?: string
+  }
